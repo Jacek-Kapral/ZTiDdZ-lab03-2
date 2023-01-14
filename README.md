940350a517a68d5fad21d3181e2589d8c244827e
Jacek-Kapral <jacek.kapral@proton.me>
2023-01-14 10:23:07 +0100

    Modification that added a level of randomness in password generation - chooses the length between 8 and 128 characters.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Second excercise from lab no. 3 in Identity and Access Management course
 Script that in first version uses generation of mix ascii characters, with obligatory usage of lowercase and uppercase letters,
 digits and special characters, with added suffling at the end so it would be more random. Generates 8 character strings. 
 Not really a safe password, to be honest.
+
+In second version i've added min and max length between 8 and 128 characters, and a function that chooses a random number of characters
+in that range and generates a password. Now i'm sure passwords generated are pretty safe.
--- lab03-2.py
+++ lab03-2.py
@@ -1,17 +1,18 @@
 import random
 import string
 
-def generuj_haslo(length):
+def generuj_haslo(dlugoscmin = 8, dlugoscmax = 128):
     znaki_ascii = string.ascii_letters + string.digits + string.punctuation
     haslo = ""
     haslo += random.choice(string.ascii_uppercase)
     haslo += random.choice(string.ascii_lowercase)
     haslo += random.choice(string.digits)
     haslo += random.choice(string.punctuation)
-    for i in range(length - 4):
+    dlugosc_hasla = random.randint(dlugoscmin, dlugoscmax)
+    for i in range(dlugosc_hasla):
         haslo += random.choice(znaki_ascii)
     haslo = ''.join(random.sample(haslo, len(haslo)))
     return haslo
 
-haslo = generuj_haslo(8)
+haslo = generuj_haslo()
 print(haslo)
